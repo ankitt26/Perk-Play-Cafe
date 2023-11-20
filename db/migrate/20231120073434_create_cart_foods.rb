bd6b8756036076a5c0d1855502b499b940177a38
Ankit <ankit26kushwa@gmail.com>
2023-11-20 13:09:08 +0530

    Create Cart_foods migration

---
+++ db/migrate/20231120073434_create_cart_foods.rb
@@ -0,0 +1,10 @@
+class CreateCartFoods < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cart_foods do |t|
+
+      t.references :cart, null: false, foreign_key: true
+      t.references :food, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
