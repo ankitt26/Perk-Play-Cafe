ec0d888334d358795e1d1b5e2b1f108f767a62a3
Ankit <ankit26kushwa@gmail.com>
2023-11-20 12:12:30 +0530

    Add columns and user FK in address table

--- db/migrate/20231120058976_create_addresses.rb
+++ db/migrate/20231120058976_create_addresses.rb
@@ -1,11 +1,15 @@
 class CreateAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :addresses do |t|
+      t.string :landmark
       t.string :street
       t.string :city
       t.integer :pincode
       t.string :state
       t.string :country
+      t.integer :phone
+      
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
