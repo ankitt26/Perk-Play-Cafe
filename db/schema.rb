8997dcb6110f260865be5ebbf7b2a29938f8ef25
Ankit <ankit26kushwa@gmail.com>
2023-11-20 12:12:41 +0530

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_15_080728) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_20_058976) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "addresses", force: :cascade do |t|
+    t.string "landmark"
     t.string "street"
     t.string "city"
     t.integer "pincode"
     t.string "state"
     t.string "country"
+    t.integer "phone"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
   create_table "foods", force: :cascade do |t|
@@ -33,4 +37,25 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_15_080728) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.integer "username", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  add_foreign_key "addresses", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user1 = User.create(name: "Ankit", username: "user1" , email: "ank@g,mail.com", password: "ank1234")
+Food.create(name:"Bravo top" ,price:300 , cooking_time:15 , quantity: 250)
+Address.create(
+    landmark: "Landmark1",
+    street: "123 Main St",
+    city: "City1",
+    pincode: 12345,
+    state: "State1",
+    country: "Country1",
+    phone: 1234567890,
+    user_id: user1.id
+  )
