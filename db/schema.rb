5f17fba9be99bc17937b1f66be0048fa6709ce35
Ankit <ankit26kushwa@gmail.com>
2023-11-24 10:14:34 +0530

    Fix username data type in user Table

--- db/schema.rb
+++ db/schema.rb
@@ -73,7 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_22_170808) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
-    t.integer "username", null: false
+    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
