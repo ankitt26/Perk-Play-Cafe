969cd9483de2cf0f82deb168e50a512f3e5c1326
Ankit <ankit26kushwa@gmail.com>
2023-11-24 11:28:19 +0530

    Change phone number type in address table from int to bigint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_22_170808) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_24_055357) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_22_170808) do
     t.integer "pincode"
     t.string "state"
     t.string "country"
-    t.integer "phone"
+    t.bigint "phone"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
