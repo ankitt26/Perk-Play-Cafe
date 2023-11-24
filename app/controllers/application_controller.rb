59a82f378a0f5bbc086e6cbbade1723aaf285db3
Ankit <ankit26kushwa@gmail.com>
2023-11-24 11:03:39 +0530

    Authenticate user before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::API
+  
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
   protected
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name])
   end
 end
+
