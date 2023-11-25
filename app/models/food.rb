8d98f44486fd7bf572f1fe7fb6a732cff1e063c6
Ankit <ankit26kushwa@gmail.com>
2023-11-25 22:19:06 +0530

    Add validation in Food Model

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,4 +4,8 @@ class Food < ApplicationRecord
     
     has_many :order_foods
     has_many :orders, through: :order_foods  
+
+    validates :name, presence: true, length: { maximum: 255 } 
+    validates :price, numericality: { only_integer: true, less_than_or_equal_to: 10000 } 
+    validates :cooking_time, :quantity, numericality: { only_integer: true, less_than_or_equal_to: 60 } 
 end
