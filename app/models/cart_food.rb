6033dcf37a23d36e8269c1065843ded50306662f
Ankit <ankit26kushwa@gmail.com>
2023-11-25 22:21:02 +0530

    Add validation in CartFood Model

--- app/models/cart_food.rb
+++ app/models/cart_food.rb
@@ -1,4 +1,8 @@
 class CartFood < ApplicationRecord
     belongs_to :cart
     belongs_to :food
+
+    validates :cart_id, presence: true, numericality: { only_integer: true }
+    validates :food_id, presence: true, numericality: { only_integer: true }
+  
 end
