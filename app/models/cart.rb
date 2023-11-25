7e19aea547e1211dbf7f68eac03990b60ec4344c
Ankit <ankit26kushwa@gmail.com>
2023-11-25 21:08:38 +0530

    Add Many to many relation b/w food and cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,6 @@
 class Cart < ApplicationRecord
+    belongs_to :user
+   
+    has_many :cart_foods 
+    has_many :foods, through: :cart_foods
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,7 @@
 class Food < ApplicationRecord
+    has_many :cart_foods
+    has_many :carts, through: :cart_foods
+    
+    has_many :order_foods
+    has_many :orders, through: :order_foods  
 end
