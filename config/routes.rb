fed6a2bc8bfac11574c68bad737f53d9183180a9
Ankit <ankit26kushwa@gmail.com>
2023-11-28 00:48:53 +0530

    Create cart_foods controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   resources :carts
   resources :addresses
   resources :foods
+  resources :cart_foods
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
