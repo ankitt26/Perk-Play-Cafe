ac08be2788ff97a62c1869a1dbdef25e0b42260f
Ankit <ankit26kushwa@gmail.com>
2023-12-04 21:35:57 +0530

    Remove validation to fix admin_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,16 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   include Devise::JWT::RevocationStrategies::JTIMatcher
   after_create :create_initial_cart
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable,
          :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
-
+         
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_one :cart, dependent: :destroy
 
-  validates :name, :username, :email, :password, presence: true
+  validates :name, :username, :email, presence: true
 
   private
 
